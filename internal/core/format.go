45dffb2f7881ff6f85bbad9d2b22303de67f4726
LDprg <71488985+LDprg@users.noreply.github.com>
2024-12-06 23:34:17 +0100

    Update format.go

--- internal/core/format.go
+++ internal/core/format.go
@@ -54,7 +54,7 @@ var CommentsByNormedExt = map[string]map[string]string{
 		"blockEnd":   `(.*-\})`,
 	},
 	".zig": {
-		"inline":     `(/{2,3}.+)`,
+		"inline":     `(/{2,3}.+)|(//!.+)`,
 		"blockStart": `$^`,
 		"blockEnd":   `$^`,
 	},
